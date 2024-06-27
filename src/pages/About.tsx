b58c1b70f81a60901c6cbf6a45145749a1597a52
Priyanshu <sethipriyanshu@outlook.com>
2024-06-27 07:58:28 -0500

    add window mockup

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,36 +2,16 @@ import aboutMeImage from '../about-me.jpg';
 
 export const About = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
-                <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
-                    <div className="mockup-phone">
-                        <div className="camera"></div> 
-                        <div className="display">
-                            <div className="artboard artboard-demo phone-1">
-                                <div className="w-full h-full carousel rounded-box">
-                                    <div className="carousel-item w-full h-full">
-                                        <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="w-full h-full object-cover" alt="Tailwind CSS Carousel component" />
-                                    </div> 
-                                    <div className="carousel-item w-full h-full">
-                                        <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" className="w-full h-full object-cover" alt="Tailwind CSS Carousel component" />
-                                    </div> 
-                                    {/* Add more carousel items as needed */}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <h1 className="text-5xl font-bold">About Me</h1>
-                    <p className="py-6">I'm a Computer Science major at The University of Alabama, with an interest in Software Engineering. My academic journey is complemented by a passion for System Design, Data Structures, and Algorithms.<br/><br/> Being highly involved with undergraduate research, I have co-authored two papers that explore the intersections of Robotics and Physics with Artificial Intelligence.<br/><br/> Beyond academia, I am an avid traveler with an interest in cultural exploration. Having ventured into 6 countries, I have relished immersing myself in diverse landscapes and culinary delights, enriching my perspective on global diversity.<br/><br/> Through this website, I aspire to offer a glimpse into my Journey towards becoming a better Software Engineer one commit at a time.</p>
-                    <button className="btn btn-outline">Github</button>
-                    <span style={{ marginRight: '10px' }}></span>
-                    <button className="btn btn-outline btn-info">LinkedIn</button>
-
-                    
-                </div>
-            </div>
+        <div>
+            <div className='about-image'>
+            <div className="mockup-browser bg-base-300 border">
+  <div className="mockup-browser-toolbar">
+    <div className="input">About Me</div>
+  </div>
+  
+  <div className="bg-base-200 flex justify-center px-4 py-16"><p>Hello! I am Priyanshu Sethi, A passionate software developer specializing in full stack development. I believe in continuous learning and am always in the middle of learning a new technology. I'm in the middle of pursuing my Bachelor's in Computer Science and am an active researcher with 2 Publications. I'm based in The United States and currently seeking internship opportunities.</p></div>
+</div>
+</div>
         </div>
     );
 };
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,54 @@
-export const Projects = () =>{
-    return(
-        <div>
-            <p>This is the Projects Page</p>
+export const Projects = () => {
+    return (
+      <div className="flex justify-center space-x-4">
+       <div className="card glass w-96">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+      alt="car!" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Life hack</h2>
+    <p>How to park your car at your garage?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Learn now!</button>
+    </div>
+  </div>
+</div>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure className="px-10 pt-10">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    );
+  };
+  
